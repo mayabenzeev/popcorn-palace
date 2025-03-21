7a3f911723a1653c75e2f6338924c82510c5e02d
Maya Benzeev <maya.benzeev@post.runi.ac.il>
2025-03-21 21:23:37 +0200

    db testing

--- src/main/java/com/att/tdp/popcorn_palace/PopcornPalaceApplication.java
+++ src/main/java/com/att/tdp/popcorn_palace/PopcornPalaceApplication.java
@@ -1,13 +1,29 @@
 package com.att.tdp.popcorn_palace;
 
+import com.att.tdp.popcorn_palace.entity.Movie;
+import com.att.tdp.popcorn_palace.repository.MovieRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PopcornPalaceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PopcornPalaceApplication.class, args);
+	}
+	@Bean
+	CommandLineRunner run(MovieRepository movieRepository) {
+		return args -> {
+			Movie movie = new Movie();
+			movie.setTitle("Test Movie");
+			movie.setGenre("Test");
+			movie.setDuration(120);
+			movie.setRating(6.1f);
+			movie.setReleaseYear(2001);
+			movieRepository.save(movie);
+		};
 	}
 
 }
