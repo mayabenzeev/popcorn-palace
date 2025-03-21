04a5d4481544c473440ecc88ee26f5618712f386
Maya Benzeev <maya.benzeev@post.runi.ac.il>
2025-03-21 21:21:59 +0200

    business rules added

--- src/main/java/com/att/tdp/popcorn_palace/repository/BookingRepository.java
+++ src/main/java/com/att/tdp/popcorn_palace/repository/BookingRepository.java
@@ -3,6 +3,9 @@ package com.att.tdp.popcorn_palace.repository;
 import com.att.tdp.popcorn_palace.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+    Optional<Booking> findByShowTimeIdAndSeatNumber(Long showTimeId, Integer seatNumber);
 
 }
--- src/main/java/com/att/tdp/popcorn_palace/repository/ShowTimeRepository.java
+++ src/main/java/com/att/tdp/popcorn_palace/repository/ShowTimeRepository.java
@@ -2,6 +2,20 @@ package com.att.tdp.popcorn_palace.repository;
 
 import com.att.tdp.popcorn_palace.entity.ShowTime;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.OffsetDateTime;
+import java.util.Optional;
 
 public interface ShowTimeRepository extends JpaRepository<ShowTime, Long> {
+
+    // find overlapping show times in the same theater
+    @Query("""
+            SELECT st FROM ShowTime st 
+            WHERE s.theater = :theater
+            AND (s.startTime < :endTime AND s.endTime > :startTime)""")
+    Optional<ShowTime> findOverlappingShowTimes(@Param("theater") String theater,
+                                                @Param("startTime") OffsetDateTime startTime,
+                                                @Param("endTime")OffsetDateTime endTime);
 }
