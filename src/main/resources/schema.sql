8367d2afd320f19b8b92128c2c342e630fc01247
Maya Benzeev <maya.benzeev@post.runi.ac.il>
2025-03-23 22:11:30 +0200

    bugfixes

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -15,8 +15,8 @@ CREATE TABLE showtimes (
     id SERIAL PRIMARY KEY,
     price FLOAT NOT NULL,
     theater VARCHAR(255) NOT NULL,
-    start_time TIMESTAMPZ NOT NULL,
-    end_time TIMESTAMPZ NOT NULL,
+    start_time TIMESTAMPTZ NOT NULL,
+    end_time TIMESTAMPTZ NOT NULL,
     movie_id INTEGER NOT NULL,
     CONSTRAINT fk_showtime_movie FOREIGN KEY (movie_id) REFERENCES movies(id) ON DELETE CASCADE
 );
